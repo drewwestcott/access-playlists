dbaf31996bde842ccb612db26595e4d2096303ba
Drew Westcott <code@drewwestcott.co.uk>
2017-04-03 23:22:10 +0100

    Authorisation being requested and playlists being returned to tableView

---
+++ access-playlist/PlaylistCell.swift
@@ -0,0 +1,17 @@
+//
+//  playlistCell.swift
+//  access-playlist
+//
+//  Created by Drew Westcott on 03/04/2017.
+//  Copyright © 2017 Drew Westcott. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistCell: UITableViewCell {
+	
+	@IBOutlet weak var playlistName: UILabel!
+	
+	
+	
+}
--- access-playlist/ViewController.swift
+++ access-playlist/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import MediaPlayer
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	
+	@IBOutlet weak var playlistTable: UITableView!
+	@IBOutlet weak var accessError: UITextView!
+	
+	var mediaAvailable: Bool!
+	var mediaPlayer = MPMediaLibrary()
+	var availablePlaylists = [String]()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		accessError.isHidden = true
+		playlistTable.delegate = self
+		playlistTable.dataSource = self
+		
+		checkMediaAccess()
+		if mediaAvailable {
+			displayPlaylists()
+		}
+	
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -20,6 +36,55 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	func checkMediaAccess() {
+	
+		let mediaAccess = MPMediaLibrary.authorizationStatus()
+		switch (mediaAccess) {
+		case MPMediaLibraryAuthorizationStatus.authorized:
+				print("Media Access has been given")
+			mediaAvailable = true
+		case MPMediaLibraryAuthorizationStatus.denied, MPMediaLibraryAuthorizationStatus.restricted:
+				print("Unable to access your media library, we will not be able to play music for you.")
+				accessError.isHidden = false
+		case MPMediaLibraryAuthorizationStatus.notDetermined:
+				MPMediaLibrary.requestAuthorization({ (MPMediaLibraryAuthorizationStatus) in
+					print("Requesting Access")
+				})
+		}
+		
+	}
+	
+	func displayPlaylists() {
+		
+		let myPlaylistQuery = MPMediaQuery.playlists()
+		let playlists = myPlaylistQuery.collections
+		
+		for playlist in playlists! {
+			let playlistFound = playlist.value(forProperty: MPMediaPlaylistPropertyName)!
+			
+			print(playlistFound)
+			availablePlaylists.append(playlistFound as! String)
+		}
+	}
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		
+		return 1
+		
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		
+		return availablePlaylists.count
+		
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		let cell = playlistTable.dequeueReusableCell(withIdentifier: "playlistCell", for: indexPath) as! PlaylistCell
+		cell.playlistName.text = availablePlaylists[indexPath.row]
+		return cell
+	}
 
 }
 
